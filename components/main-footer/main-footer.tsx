33506d2d3d4aa9d28a11d025ca0b270f114feef1
Raphael <raphaelmarcelino2798@gmail.com>
2026-07-13 22:50:52 +0800

    Safe area for safari

--- components/main-footer/main-footer.tsx
+++ components/main-footer/main-footer.tsx
@@ -1,6 +1,6 @@
 export default function MainFooter() {
   return (
-    <footer className="w-full bg-[#2F6F68] border-t border-white/5 py-6 text-center text-xs text-[#ffffff]">
+    <footer className="w-full bg-[#2F6F68] border-t border-white/5 py-6 text-center text-xs text-white pb-[calc(1.5rem+env(safe-area-inset-bottom))] pt-6">
       © 2004 FILSTAR Real Estate Brokerage. All rights reserved.
     </footer>
   );
